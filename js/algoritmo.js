3de26bdea0d3ba0b8af8dd6cdaab9c2e61b82237
GamerCleanVic <jonassd2000@gmail.com>
2022-05-27 22:49:03 -0500

    +indexHTML, +styleCSS

--- js/algoritmo.js
+++ js/algoritmo.js
@@ -2,8 +2,12 @@ function inicial(){
     var a = 1;
     var b = 3;
     var c = a = b;
+    const p1 = document.getElementById("oPrint");
 }
 inicial();
 if(a && b != 0){
-    alert(a+" + "+b+" = "c+);
+    p1.insertAdjacentHTML(a+" + "+b+" = "+c);
+}
+else{
+    alert("O 2 nºs devem ser diferentes de zero!");
 }
